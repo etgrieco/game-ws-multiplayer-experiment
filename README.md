3426ec1d9a5296b4ff0be8ced5cef4ad651b942b
Elliott Grieco <elliott.grieco@gmail.com>
2025-03-04 13:59:20 -0500

    Delete erroneous output files

---
+++ README.md
@@ -0,0 +1,3 @@
+# Dev
+
+To run client + server together: `pnpm run -r dev`
--- shared/ecs/trait.js
+++
@@ -1,3 +0,0 @@
-import { trait } from "koota";
-export const Position2 = trait({ x: 0, y: 0 });
-export const Velocity2 = trait({ x: 0, y: 0 });
