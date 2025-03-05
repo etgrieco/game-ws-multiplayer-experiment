abbf3f0f48bbcf95106af2107bb620cdf043845e
Elliott Grieco <elliott.grieco@gmail.com>
2025-03-05 14:13:59 -0500

    Refactor, basic threejs + multiplayer integration

---
+++ client/src/net/gameSession.tsx
@@ -0,0 +1,158 @@
+import { Position2, Velocity2 } from "@shared/ecs/trait";
+import { GameSimulation } from "@shared/game/types";
+import {
+  GameSessionClientEvent,
+  GameSessionServerEvent,
+} from "@shared/net/messages";
+import { createWorld } from "koota";
+import { createStore } from "zustand";
+
+export type WsStore = {
+  game: GameSimulation | null;
+  initGameError: {
+    message: string;
+  } | null;
+  ws: WebSocket | null;
+  initWs: () => void;
+  removeWs: () => void;
+  createSession: () => void;
+  joinSession: (params: { sessionId: string }) => void;
+};
+
+export const gameSessionStore = createStore<WsStore>()((set, getStore) => {
+  return {
+    game: null,
+    initGameError: null,
+    ws: null,
+    removeWs() {
+      return set((state) => {
+        state.ws?.close();
+        return {
+          ws: null,
+        };
+      });
+    },
+    initWs() {
+      return set(function setupWs(state) {
+        if (state.ws) {
+          throw new Error(
+            "WS already set! make sure to get rid of it first...",
+          );
+        }
+        const ws = new WebSocket("ws://localhost:8080");
+
+        ws.addEventListener("open", function () {
+          console.log("connected to the server");
+        });
+
+        ws.addEventListener("close", function () {
+          console.log("server connection closed");
+          // attempt to re-connect! (TODO: avoid infinite loop? use a flag?)
+          state.initWs();
+        });
+
+        ws.addEventListener("message", function (e) {
+          console.log(`Received message: `, e.data);
+        });
+
+        ws.addEventListener("message", function (e) {
+          console.log("received data", e);
+          if (typeof e.data === "string") {
+            try {
+              const jsonData = JSON.parse(e.data) as GameSessionServerEvent;
+              switch (jsonData.type) {
+                case "CREATE_SESSION_RESPONSE":
+                  {
+                    set(() => {
+                      // TODO: actually set up world
+                      const world = createWorld();
+                      world.spawn(Position2, Velocity2);
+
+                      return {
+                        game: {
+                          tick() {
+                            // TODO: actually set up tick
+                          },
+                          data: {
+                            id: jsonData.data.id,
+                            world: world,
+                          },
+                        },
+                      };
+                    });
+                  }
+                  break;
+                case "JOIN_SESSION_RESPONSE":
+                  if (jsonData.data.success) {
+                    const data = jsonData.data;
+                    // TODO: actually set up world
+                    const world = createWorld();
+                    world.spawn(Position2, Velocity2);
+                    set({
+                      game: {
+                        data: {
+                          id: data.game.id,
+                          world: world,
+                        },
+                        tick() {
+                          // TODO: actually set up tick
+                        },
+                      },
+                    });
+                  } else {
+                    const data = jsonData.data;
+                    set({
+                      initGameError: {
+                        message: data.failure,
+                      },
+                    });
+                  }
+                  break;
+                default: {
+                  const jsonUnknown: unknown = jsonData;
+                  const jsonUnknownType =
+                    jsonUnknown &&
+                    typeof jsonUnknown === "object" &&
+                    "type" in jsonUnknown &&
+                    typeof jsonUnknown.type === "string"
+                      ? jsonUnknown.type
+                      : undefined;
+                  console.warn(
+                    `Unhandled server event${jsonUnknownType ? `, ${jsonUnknownType}` : ""}`,
+                  );
+                }
+              }
+            } catch (e) {
+              console.error(e);
+            }
+          }
+        });
+
+        return { ws };
+      });
+    },
+    // START WS APIs
+    createSession() {
+      const { ws } = getStore();
+      if (!ws) throw new Error("WS not established!");
+      wsSend(ws, {
+        type: "CREATE_SESSION",
+        data: undefined,
+      });
+    },
+    joinSession(params) {
+      const { ws } = getStore();
+      if (!ws) throw new Error("WS not established!");
+      wsSend(ws, {
+        type: "JOIN_SESSION",
+        data: {
+          id: params.sessionId,
+        },
+      });
+    },
+  };
+});
+
+function wsSend(ws: WebSocket, msg: GameSessionClientEvent): void {
+  ws.send(JSON.stringify(msg));
+}
---
+++ client/src/ui/App.tsx
@@ -0,0 +1,131 @@
+import React, { useRef } from "react";
+import { useStore } from "zustand";
+import * as THREE from "three";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { gameSessionStore } from "../net/gameSession";
+
+export function App() {
+  const ws = useStore(gameSessionStore, (s) => s.ws);
+  const game = useStore(gameSessionStore, (s) => s.game);
+  const initGameError = useStore(gameSessionStore, (s) => s.initGameError);
+  const createSession = useStore(gameSessionStore, (s) => s.createSession);
+  const joinSession = useStore(gameSessionStore, (s) => s.joinSession);
+
+  React.useEffect(() => {
+    const state = gameSessionStore.getState();
+
+    if (!state.ws) {
+      state.initWs();
+    }
+    return () => {
+      state.removeWs();
+    };
+  }, []);
+
+  if (initGameError) {
+    return <div>Error: {initGameError.message}</div>;
+  }
+
+  if (game) {
+    return <Game />;
+  }
+
+  return (
+    <div className="flex flex-col gap-4 max-w-[1024px] items-center w-full  ">
+      <button
+        disabled={!ws}
+        onClick={createSession}
+        className="h-9 px-4 py-2 bg-green-600 text-green bg-green-600-foreground shadow hover:bg-green-600/90 rounded-sm"
+      >
+        Create Session
+      </button>
+
+      <div className="text-lg">OR</div>
+
+      <div>
+        <div>Join a Session</div>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            e.stopPropagation();
+
+            if (!ws) {
+              throw new Error("WS connection unavailable");
+            }
+
+            const values = new FormData(e.target as HTMLFormElement);
+            const sessionId = values.get("sessionId") as string;
+            joinSession({ sessionId });
+          }}
+        >
+          <div className="flex flex-col gap-2">
+            <fieldset>
+              <label htmlFor="sessionId">Session ID: </label>
+              <input
+                className="flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm"
+                id="sessionId"
+                name="sessionId"
+                type="text"
+              ></input>
+            </fieldset>
+            <button className="h-9 px-4 py-2 bg-green-600 text-green bg-green-600-foreground shadow hover:bg-green-600/90 rounded-sm">
+              Join
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+const useGame = () => {
+  const game = useStore(gameSessionStore, (s) => s.game);
+  if (!game) {
+    throw new Error("The game is not yet initialized");
+  }
+  return game;
+};
+
+function GameUI() {
+  const game = useGame();
+  return (
+    <div className="w-full max-w-[1024px] max-h-[768px] h-full absolute">
+      <div className="text-2xl">GAME ID: {game.data.id}</div>
+    </div>
+  );
+}
+
+function Game() {
+  return (
+    <div className="flex flex-col w-full max-w-[1024px] relative">
+      <Canvas>
+        <ambientLight intensity={Math.PI / 2} />
+        <spotLight
+          position={[10, 10, 10]}
+          angle={0.15}
+          penumbra={1}
+          decay={0}
+          intensity={Math.PI}
+        />
+        <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+        <GameContents />
+      </Canvas>
+      <GameUI />
+    </div>
+  );
+}
+
+function GameContents() {
+  const meshRef = useRef<THREE.Mesh>(null!);
+
+  useFrame((_s, delta) => {
+    meshRef.current.rotation.x += delta;
+  });
+
+  return (
+    <mesh ref={meshRef} scale={1}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={"orange"} />
+    </mesh>
+  );
+}
