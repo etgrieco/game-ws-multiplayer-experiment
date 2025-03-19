0e3bbd75f50e7454cd0158a945c6da6b3a86c147
Elliott Grieco <elliott.grieco@gmail.com>
2025-03-18 20:48:39 -0400

    Controls tweaks

--- client/src/ui/Game.tsx
+++ client/src/ui/Game.tsx
@@ -5,7 +5,7 @@ import React, { useRef } from "react";
 // biome-ignore lint/style/useImportType: Let this change over time...
 import * as THREE from "three";
 import { OrbitControls, Stats } from "@react-three/drei";
-import { useControls } from "leva";
+import { folder, useControls } from "leva";
 
 export function Game() {
   return (
@@ -68,6 +68,28 @@ function Terrain(props: { width: number; height: number }) {
   );
 }
 
+const trees: { id: string; posX: number; posZ: number }[] = [
+  {
+    id: window.crypto.randomUUID(),
+    posX: 0,
+    posZ: 0,
+  },
+  {
+    id: window.crypto.randomUUID(),
+    posX: 1,
+    posZ: 1,
+  },
+];
+
+function Tree(props: { posX: number; posZ: number }) {
+  return (
+    <mesh position={[props.posX, 0.5, props.posZ]}>
+      <coneGeometry args={[0.2, 1, 8]} />
+      <meshStandardMaterial color={0x305010} flatShading />
+    </mesh>
+  );
+}
+
 const playerColors = [0x00ff00, 0xff0000];
 
 function GameContents() {
@@ -79,10 +101,10 @@ function GameContents() {
 
   return (
     <>
-      <OrbitControls />
+      <ControlledOrbitControl />
       <Stats />
       <directionalLight position={[1, 1, 1]} />
-      <ambientLight intensity={0.5} />
+      <ambientLight intensity={0.8} />
       <scene>
         {players.map((p, idx) => {
           return (
@@ -94,7 +116,43 @@ function GameContents() {
           );
         })}
         <Terrain width={terrainWidth} height={terrainHeight} />
+        {trees.map((t) => {
+          return <Tree key={t.id} posX={t.posX} posZ={t.posZ} />;
+        })}
       </scene>
     </>
+  );
+}
+
+function ControlledOrbitControl() {
+  const [_cameraVals, setCamera] = useControls(() => {
+    return {
+      Camera: folder({
+        posX: 0,
+        posY: 0,
+        posZ: 0,
+        rotX: 0,
+        rotY: 0,
+        rotZ: 0,
+      }),
+    };
+  });
+
+  return (
+    <OrbitControls
+      onChange={(self) => {
+        const pos = self?.target.object.position;
+        const rot = self?.target.object.rotation;
+        if (!pos || !rot) return;
+        setCamera({
+          posX: pos.x,
+          posY: pos.y,
+          posZ: pos.z,
+          rotX: rot.x,
+          rotY: rot.y,
+          rotZ: rot.z,
+        });
+      }}
+    />
   );
 }
